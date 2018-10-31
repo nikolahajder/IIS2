671ca691d984dbba299e7b5ebc52dd2bae15c1d7
markobojkic <marko.bojkic@uns.ac.rs>
2018-10-31 09:41:02 +0100

    Vezbe 3

---
+++ IIS2/src/geometry/Rectangle.java
@@ -0,0 +1,41 @@
+package geometry;
+
+public class Rectangle {
+
+	private Point upperLeftPoint;
+	private int width;
+	private int height;
+	private boolean selected;
+	
+	public int area() {
+		return width * height;
+	}
+	
+	public Point getUpperLeftPoint() {
+		return upperLeftPoint;
+	}
+	public void setUpperLeftPoint(Point upperLeftPoint) {
+		this.upperLeftPoint = upperLeftPoint;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public boolean isSelected() {
+		return selected;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	
+	
+	
+}
--- IIS2/src/geometry/Test.java
+++ IIS2/src/geometry/Test.java
@@ -16,6 +16,34 @@ public class Test {
 		double result = p.distance(56, 87);
 		System.out.println("Distance between points is: " + result);
 		
+		Point p1 = new Point();
+		p1.setX(18);
+		p1.setY(30);
+		
+		// 1. Inicijalizovati x koordinatu tacke p
+		// na vrednost y koordinate tacke p1
+
+		// 2. Postaviti za pocetnu tacku linije l1 tacku p, a
+		// za krajnju tacku linije l1 tacku p1
+
+		// 3. Postaviti y koordinatu krajnje tacke l1 na 23
+
+		// 4. Inicijalizovati x koordinatu pocetne tacke linije l1
+		// na vrednost y koordinate krajnje tacke linije l1
+
+		// 5. Postaviti x koordinatu krajnje tacke l1 na vrednost
+		// duzine linije l1 umanjene za vrednost zbira x i y
+		// koordinate pocetne tacke linije
+
+		// 6. Postaviti x koordinatu tacke gore levo pravougaonika
+		// r1 na vrednost 10 i y koordinatu na vrednost 15
+
+		// 7. Postaviti centar kruga c1 na koordinate tacke
+		// gore levo od r1
+
+		// 8. Postaviti x koordinatu centra kruga na vrednost razlike
+		// povrsine pravougaonika i y koordinate pocetne tacke linije l1
+		
 	}
 
 }
