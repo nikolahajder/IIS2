2614f12f428203fb0d501eba16c42b2749161591
markobojkic <marko.bojkic@uns.ac.rs>
2018-12-26 09:49:15 +0100

    Vezbe 11

---
+++ IIS2/src/student/DlgStudent.java
@@ -0,0 +1,214 @@
+package student;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class DlgStudent extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JButton btnPotvrdi;
+	private JButton btnOdustani;
+	private JLabel lblIme;
+	private JTextField txtIme;
+	private JTextField txtPrezime;
+	private JTextField txtBrojIndeksa;
+	boolean isOk;
+	private JComboBox cbxSmer = new JComboBox();
+	private JComboBox cbxGodina = new JComboBox();
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			DlgStudent dialog = new DlgStudent();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public DlgStudent() {
+		setModal(true);
+		setResizable(false);
+		setTitle("Unos/modifikacija studenta");
+		setBounds(100, 100, 257, 324);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		lblIme = new JLabel("Ime:");
+		lblIme.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		
+		txtIme = new JTextField();
+		txtIme.setColumns(10);
+		
+		JLabel lblPrezime = new JLabel("Prezime:");
+		lblPrezime.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		
+		txtPrezime = new JTextField();
+		txtPrezime.setColumns(10);
+		
+		
+		cbxSmer.addItem("IT");
+		cbxSmer.addItem("IM");
+		cbxSmer.addItem("MH");
+		
+		txtBrojIndeksa = new JTextField();
+		txtBrojIndeksa.setColumns(10);
+		
+		JLabel lblC = new JLabel("/");
+		lblC.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblC.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		
+		cbxGodina.addItem("2013");
+		cbxGodina.addItem("2014");
+		cbxGodina.addItem("2015");
+		
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addGap(19)
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblPrezime, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblIme, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtPrezime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtIme, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(cbxSmer, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtBrojIndeksa, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(lblC, GroupLayout.PREFERRED_SIZE, 8, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(cbxGodina, 0, 63, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblIme, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtIme, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblPrezime, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtPrezime, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+					.addGap(26)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(cbxSmer, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtBrojIndeksa, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblC)
+						.addComponent(cbxGodina, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(63, Short.MAX_VALUE))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				btnPotvrdi = new JButton("Potvrdi");
+				btnPotvrdi.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						isOk = true;
+						setVisible(false);
+					}
+				});
+				btnPotvrdi.setActionCommand("OK");
+				getRootPane().setDefaultButton(btnPotvrdi);
+			}
+			{
+				btnOdustani = new JButton("Odustani");
+				btnOdustani.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				btnOdustani.setActionCommand("Cancel");
+			}
+			GroupLayout gl_buttonPane = new GroupLayout(buttonPane);
+			gl_buttonPane.setHorizontalGroup(
+				gl_buttonPane.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_buttonPane.createSequentialGroup()
+						.addGap(41)
+						.addComponent(btnPotvrdi)
+						.addGap(18)
+						.addComponent(btnOdustani)
+						.addGap(40))
+			);
+			gl_buttonPane.setVerticalGroup(
+				gl_buttonPane.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_buttonPane.createSequentialGroup()
+						.addGap(5)
+						.addGroup(gl_buttonPane.createParallelGroup(Alignment.BASELINE)
+							.addComponent(btnPotvrdi)
+							.addComponent(btnOdustani)))
+			);
+			buttonPane.setLayout(gl_buttonPane);
+		}
+	}
+
+	public JTextField getTxtIme() {
+		return txtIme;
+	}
+
+	public void setTxtIme(JTextField txtIme) {
+		this.txtIme = txtIme;
+	}
+
+	public JTextField getTxtPrezime() {
+		return txtPrezime;
+	}
+
+	public void setTxtPrezime(JTextField txtPrezime) {
+		this.txtPrezime = txtPrezime;
+	}
+
+	public JTextField getTxtBrojIndeksa() {
+		return txtBrojIndeksa;
+	}
+
+	public void setTxtBrojIndeksa(JTextField txtBrojIndeksa) {
+		this.txtBrojIndeksa = txtBrojIndeksa;
+	}
+
+	public JComboBox getCbxSmer() {
+		return cbxSmer;
+	}
+
+	public void setCbxSmer(JComboBox cbxSmer) {
+		this.cbxSmer = cbxSmer;
+	}
+
+	public JComboBox getCbxGodina() {
+		return cbxGodina;
+	}
+
+	public void setCbxGodina(JComboBox cbxGodina) {
+		this.cbxGodina = cbxGodina;
+	}
+}
---
+++ IIS2/src/student/FrmStudent.java
@@ -0,0 +1,146 @@
+package student;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultListModel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FrmStudent extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtBrojStudenata;
+	DefaultListModel dlm = new DefaultListModel();
+	int brojac = 0;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmStudent frame = new FrmStudent();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmStudent() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel pnlZapad = new JPanel();
+		contentPane.add(pnlZapad, BorderLayout.WEST);
+		
+		JButton btnDodaj = new JButton("Dodaj");
+		btnDodaj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DlgStudent dlgStudent = new DlgStudent();
+				dlgStudent.setVisible(true);
+				if (dlgStudent.isOk) {
+					dlm.addElement("Broj indeksa: " + dlgStudent.getCbxSmer().getSelectedItem() + " "
+							+ dlgStudent.getTxtBrojIndeksa().getText() + " / "
+							+ dlgStudent.getCbxGodina().getSelectedItem() + ", Prezime "
+							+ dlgStudent.getTxtPrezime().getText() + " Ime " + dlgStudent.getTxtIme().getText());
+					brojac++;
+					txtBrojStudenata.setText(Integer.toString(brojac));
+				}
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("Modifikuj");
+		GroupLayout gl_pnlZapad = new GroupLayout(pnlZapad);
+		gl_pnlZapad.setHorizontalGroup(
+			gl_pnlZapad.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlZapad.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_pnlZapad.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 75, Short.MAX_VALUE)
+						.addComponent(btnDodaj, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		gl_pnlZapad.setVerticalGroup(
+			gl_pnlZapad.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlZapad.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnDodaj)
+					.addGap(18)
+					.addComponent(btnNewButton_1)
+					.addContainerGap(146, Short.MAX_VALUE))
+		);
+		pnlZapad.setLayout(gl_pnlZapad);
+		
+		JPanel pnlCentar = new JPanel();
+		contentPane.add(pnlCentar, BorderLayout.CENTER);
+		
+		JScrollPane scrlStudent = new JScrollPane();
+		GroupLayout gl_pnlCentar = new GroupLayout(pnlCentar);
+		gl_pnlCentar.setHorizontalGroup(
+			gl_pnlCentar.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlCentar.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrlStudent, GroupLayout.PREFERRED_SIZE, 235, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(13, Short.MAX_VALUE))
+		);
+		gl_pnlCentar.setVerticalGroup(
+			gl_pnlCentar.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlCentar.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrlStudent, GroupLayout.PREFERRED_SIZE, 215, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(15, Short.MAX_VALUE))
+		);
+		
+		JList lstStudent = new JList();
+		lstStudent.setModel(dlm);
+		scrlStudent.setViewportView(lstStudent);
+		pnlCentar.setLayout(gl_pnlCentar);
+		
+		JPanel pnlIstok = new JPanel();
+		contentPane.add(pnlIstok, BorderLayout.EAST);
+		
+		JButton btnUkloni = new JButton("Ukloni");
+		pnlIstok.add(btnUkloni);
+		
+		JPanel pnlJug = new JPanel();
+		contentPane.add(pnlJug, BorderLayout.SOUTH);
+		
+		JLabel lbl1 = new JLabel("Studenata u listi:");
+		pnlJug.add(lbl1);
+		
+		txtBrojStudenata = new JTextField();
+		txtBrojStudenata.setEditable(false);
+		pnlJug.add(txtBrojStudenata);
+		txtBrojStudenata.setColumns(2);
+		
+		JLabel lblEvidencijaStudenata = new JLabel("Evidencija studenata");
+		lblEvidencijaStudenata.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEvidencijaStudenata.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblEvidencijaStudenata, BorderLayout.NORTH);
+	}
+}
+
